cd3d3b36081f4b207c893993b9149a12fc58165c
cecilia reynoso <cecireynoso@gmail.com>
2023-11-06 16:24:31 -0300

    fetch the questions2

--- game.js
+++ game.js
@@ -10,32 +10,19 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let questions = [
-  {
-      question: 'Inside which HTML element do we put the JavaScript??',
-      choice1: '<script>',
-      choice2: '<javascript>',
-      choice3: '<js>',
-      choice4: '<scripting>',
-      answer: 1,
-  },
-  {
-      question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
-      choice1: "<script href='xxx.js'>",
-      choice2: "<script name='xxx.js'>",
-      choice3: "<script src='xxx.js'>",
-      choice4: "<script file='xxx.js'>",
-      answer: 3,
-  },
-  {
-      question: "How do you write 'Hello World' in an alert box?",
-      choice1: "msgBox('Hello World');",
-      choice2: "alertBox('Hello World');",
-      choice3: "msg('Hello World');",
-      choice4: "alert('Hello World');",
-      answer: 4,
-  },
-];
+let questions = []; // this will be an empty array that will hold the questions
+
+fetch("questions.json").then(res => {
+  return res.json(); // this will return the response in json format
+})
+.then(loadedQuestions => {
+  console.log(loadedQuestions);
+  questions = loadedQuestions;
+  startGame();
+}) // this will fetch the questions from the json file
+.catch(err =>{
+  console.error(err);
+})
 
 //CONSTANTS
 
@@ -103,5 +90,3 @@ incrementScore = (num) => {
   score += num;
   scoreText.innerText = score;
 }
-
-startGame();
